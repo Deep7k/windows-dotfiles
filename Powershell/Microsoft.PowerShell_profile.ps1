5fff28017f8a5ad60c29e47b148bf69f7f89ce74
DEEPAK N A <60418902+Deep7k@users.noreply.github.com>
2025-04-20 00:44:15 +0530

    Refactor code

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,18 @@
 # Set PowerShell encoding to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-#######################################################
 # Prompt Configuration
-#######################################################
-
 oh-my-posh --init --shell pwsh --config "$env:USERPROFILE\Documents\Powershell\oh-my-posh\BlueOwlNew.json" | Invoke-Expression
 
-#######################################################
-# Modules
-#######################################################
-
+# Import modules
 Import-Module -Name Terminal-Icons
 Import-Module -Name posh-git
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
 
-#######################################################
 # PSReadLine Configuration
-#######################################################
-
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
@@ -28,25 +23,15 @@ Set-PSReadLineKeyHandler -Key Ctrl+LeftArrow -Function ShellBackwardWord
 Set-PSReadLineKeyHandler -Key Ctrl+Backspace -Function ShellBackwardKillWord
 Set-PSReadLineOption -HistoryNoDuplicates:$True
 
-#######################################################
 # Aliases
-#######################################################
-
-Set-Alias npp 'notepad++'
-# Set-Alias ping Test-Connection
+Set-Alias npp 'C:\Program Files\Notepad++\notepad++.exe'
 Set-Alias wget Invoke-WebRequest
-New-Alias -Name iamhere -Value Send-Keystrokes
+New-Alias -Name iamhere -Value Send-Keystroke
 
-#######################################################
 # Environment Variables
-#######################################################
-
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 
-#######################################################
 # Autocomplete configurations
-#######################################################
-
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
   param($wordToComplete, $commandAst, $cursorPosition)
   [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
@@ -57,16 +42,8 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
   }
 }
 
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
-#######################################################
 # Custom Functions
-#######################################################
-
-function Send-Keystrokes {
+function Send-Keystroke {
   param (
     [switch]$Verbose = $false,
     [int]$KeystrokeInterval = 40,
